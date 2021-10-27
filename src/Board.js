e0a8fa6c1aaee14481ad851d644c92844431053c
Alejandro <acastillocampillo@gmail.com>
2021-10-27 11:32:06 +0200

    Trying develop

--- src/Board.js
+++ src/Board.js
@@ -125,7 +125,7 @@ class Board extends Component {
         ) : (
           <div>
             <div className='Board-title'>
-              <div className='neon-orange'>Lights</div>
+              <div className='neon-orange'>Light</div>
               <div className='neon-blue'>Out</div>
             </div>
             {this.makeTable()}
